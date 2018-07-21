677df8ac79fdc03b6b08e152fcc0b371d1bdf6e5
sineadeasdon <sineadalexandria@gmail.com>
2018-07-21 05:31:12 +0000

    First commit

---
+++ AuthDemo/app.js
@@ -0,0 +1,94 @@
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    User                    = require("./models/user"),
+    localStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+
+var app = express();
+mongoose.connect("mongodb://localhost/auth_demo");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "This is my secret message",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//======
+//ROUTES 
+//======
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/secret", isLoggedIn, function(req, res){
+   res.render("secret"); 
+});
+
+//==============
+//SIGN UP ROUTES 
+//==============
+
+//Show sign-up form
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+//Handle user sign up
+app.post("/register", function(req, res){
+   User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render("register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        }
+   });
+});
+
+//============
+//LOGIN ROUTES
+//============
+
+//Show login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+
+//Login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
+});
+
+//============
+//LOGOUT ROUTE
+//============
+
+app.get("/logout", function(req, res){
+   req.logout(); 
+   res.redirect("/");
+});
+
+function isLoggedIn (req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The AuthDemo server is running");
+});
